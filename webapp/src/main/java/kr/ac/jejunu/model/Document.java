23b04a1bdbca9740054c5896c2251a3196c998f2
WoochanKim <woochankim200@gmail.com>
2017-06-08 19:11:25 +0900

    I'm modeling data

--- webapp/src/main/java/kr/ac/jejunu/model/Document.java
+++ webapp/src/main/java/kr/ac/jejunu/model/Document.java
@@ -1,17 +1,27 @@
 package kr.ac.jejunu.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by kimwoochan on 2017-06-08.
  */
 @Entity
 public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Position startPosition;
-    private Position endPosition;
+
+    private double startLatitude;
+    private double startLongitude;
+
+    private double endLatitude;
+    private double endLongitude;
+
     private User writer;
     private String content;
+
+
 
 
 
